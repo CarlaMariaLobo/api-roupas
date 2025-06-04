0f17597b8b381d260df3119588130827cf8d32fc
Carla <carla.lobo1@aluno.ce.gov.br>
2025-06-04 08:29:28 -0300

    add use

--- vendor/brick/math/src/Exception/IntegerOverflowException.php
+++ vendor/brick/math/src/Exception/IntegerOverflowException.php
@@ -6,14 +6,10 @@ namespace Brick\Math\Exception;
 
 use Brick\Math\BigInteger;
 
-/**
- * Exception thrown when an integer overflow occurs.
- */
+
 class IntegerOverflowException extends MathException
 {
-    /**
-     * @psalm-pure
-     */
+    
     public static function toIntOverflow(BigInteger $value) : IntegerOverflowException
     {
         $message = '%s is out of range %d to %d and cannot be represented as an integer.';
