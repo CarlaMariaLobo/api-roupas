f66bb04d559e6f8102ecf8c8843433d4b9be2410
Carla <carla.lobo1@aluno.ce.gov.br>
2025-06-05 08:39:06 -0300

    add users

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,9 @@
                                 <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border dark:border-[#3E3E3A] border-[#e3e3e0]">
                                     <span class="rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A] w-1.5 h-1.5"></span>
                                 </span>
-                            </span>
+                  // Bootstrap Laravel and handle the request...
+/** @var Application $app */
+          </span>
                             <span>
                                 Watch video tutorials at
                                 <a href="https://laracasts.com" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
